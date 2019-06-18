c5fe5b370bf264eb5b6818217ce65d2b8bf0fe56
fabifors <fabian.forsstrom.com>
2019-06-18 23:31:51 +0200

    WIP commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,33 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import VueFirestore from 'vue-firestore'
+import firebase from 'firebase'
+import { firebaseConfig } from '../firebase.config.js'
+
+require('firebase/firestore')
+
+Vue.use(VueFirestore)
+
+// Initialize Firebase
+var firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const firestore = firebaseApp.firestore()
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
+/*
+var vm = new Vue({
+  el: '#app',
+  firestore () {
+    return {
+        // Collection
+        persons: firestore.collection('persons'),
+        // Doc
+        ford: firestore.collection('cars').doc('ford')
+    }
+  }
+})
+*/
