9450aba9daf60b5a511eb710e50f2906da008b20
fabifors <fabian.forsstrom.com>
2019-07-05 17:23:49 +0200

    removed old test posts and changed yml and config files

--- gridsome.config.js
+++ gridsome.config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       use: '@gridsome/source-filesystem',
       options: {
-        path: 'content/blog/*.md',
+        path: 'posts/**/*.md',
         typeName: 'Post',
         route: '/blog/:slug',
         refs: {
